5be152e3b4fad1844be91a87ef0d37baaec228cb
Luís Portela Afonso <luis.afonso@mindera.com>
2015-06-02 01:59:02 +0100

    Code review fixes
    	* Changed for loop
    	* Removed semicolons
    	* Removed unecessary !!

--- retab.safariextension/index.js
+++ retab.safariextension/index.js
@@ -11,32 +11,33 @@
        return (
         function (tab, visibility) {
 
-          var browserWindow, openTab;
+          var browserWindow;
+          var openTab;
 
           // Search for the tab browser window in the current open windows
-          for (var i = 0; i < this.browserWindows.length; i++) {
+          for (var i = this.browserWindows.length - 1; i >= 0; i--) {
             if (tab.browserWindow === this.browserWindows[i]) {
-              browserWindow = tab.browserWindow;
-              break;
+              browserWindow = tab.browserWindow
+              break
             }
-          };
+          }
 
           // Check if a browser window was found
-          if (!!!browserWindow) {
-            browserWindow = this.openBrowserWindow();
-
-            // Update the window instances of all tabs with a window closed
-            retab.updateBroserWindow(tab.browserWindow, browserWindow);
-
-            openTab = browserWindow.activeTab;
+          if (browserWindow) {
+            openTab = browserWindow.openTab(visibility, tab.index)
           }
           else {
-            openTab = browserWindow.openTab(visibility, tab.index);
+            browserWindow = this.openBrowserWindow()
+
+            // Update the window instances of all tabs with a window closed
+            retab.updateBroserWindow(tab.browserWindow, browserWindow)
+
+            openTab = browserWindow.activeTab
           }
 
           // Update tab information
-          openTab.url = tab.url;
-          openTab.index = tab.index;
+          openTab.url = tab.url
+          openTab.index = tab.index
 
         }.call(this.app, tabs.pop(), visibility))
       }
@@ -59,7 +60,7 @@
 
       tabs.forEach(function (tab, index) {
         if (tab.browserWindow === oldBrowserWindow) {
-          tab.browserWindow = newBrowserWindow;
+          tab.browserWindow = newBrowserWindow
         }
       })
     }
@@ -76,7 +77,7 @@
   // Set this callback to the tab close event
   app.addEventListener("close", function (e) {
     if (!incognito && !!e.target.url) {
-      retab.update(e.target);
+      retab.update(e.target)
     }
   }, true)
 
